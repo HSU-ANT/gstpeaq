62200facb2073c58f26cb93bceace6b7ad54059d
Martin Holters <martin.holters@hsu-hh.de>
2006-10-30 10:05:16 +0000

    Use only one source and a tee for the test with equal reference and test signal. Note: Necessacitates queues at input to peaq. Use slightly modifed comparison to catch cases in which no ODG is output at all.

--- runtest.sh
+++ runtest.sh
@@ -1,14 +1,16 @@
 ODG=`LANG=LC_ALL gst-launch-0.10 --gst-disable-segtrap --gst-debug-level=2 --gst-plugin-load=.libs/libgstpeaq.so \
 	audiotestsrc name=src0 num-buffers=128 freq=440 \
-	audiotestsrc name=src1 num-buffers=128 freq=440 \
 	audioconvert name=convert0 \
-	audioconvert name=convert1 \
-	peaq name=peaq \
-	src0.src\!convert0.sink src1.src\!convert1.sink \
-	convert0.src\!peaq.ref convert1.src\!peaq.test \
+	tee \
+	queue name=queue0 \
+	queue name=queue1 \
+	peaq name=peaq0 \
+	src0.src\!convert0.sink convert0.src\!tee0.sink \
+	tee0.src2\!queue0.sink tee0.src1\!queue1.sink \
+	queue0.src\!peaq0.ref queue1.src\!peaq0.test \
 | grep "Objective Difference Grade:" | cut -d " " -f4`
 echo $ODG
-if [ $ODG != 0.187 ]; then
+if [ x$ODG != x0.187 ]; then
 	exit 1
 fi
 ODG=`LANG=LC_ALL gst-launch-0.10 --gst-disable-segtrap --gst-debug-level=2 --gst-plugin-load=.libs/libgstpeaq.so \
@@ -21,7 +23,7 @@ ODG=`LANG=LC_ALL gst-launch-0.10 --gst-disable-segtrap --gst-debug-level=2 --gst
 	convert0.src\!peaq.ref convert1.src\!peaq.test \
 | grep "Objective Difference Grade:" | cut -d " " -f4`
 echo $ODG
-if [ $ODG != -1.993 ]; then
+if [ x$ODG != x-1.993 ]; then
 	exit 1
 fi
 exit 0
