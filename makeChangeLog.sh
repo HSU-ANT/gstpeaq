0fbf987838608aead8a1a374104d83be951b10e0
Martin Holters <martin.holters@hsu-hh.de>
2010-11-22 07:30:19 +0000

    Use new SVN prefix.

--- makeChangeLog.sh
+++ makeChangeLog.sh
@@ -1,2 +1,2 @@
-svn log -v --xml | aux/svn2log.py --prefix="/Holters/gstpeaq/trunk" --users="aux/users"
+svn log -v --xml | aux/svn2log.py --prefix="/misc/public/Holters/gstpeaq/trunk" --users="aux/users"
 
